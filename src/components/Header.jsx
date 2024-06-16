76db75f015cdf37023097527274ec08f702db7b4
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-06-16 20:17:20 +0530

    Complete the dark mode and light mode functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,21 @@ import { Button, Navbar } from "flowbite-react"
 import { FaMoon, FaSun } from "react-icons/fa";
 import ars from '/ars.png'
 import { Link, useLocation } from "react-router-dom"
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 const Header = () => {
    const path = useLocation().pathname
+   const {theme} = useSelector(state => state.theme)
+   const dispatch = useDispatch()
   return (
     <Navbar className="border-b-2 sticky top-0 bg-slate-200 shadow-md z-40">
       <div>
          <img src={ars} alt="logo" width='100' />
       </div>
       <div className="flex gap-2 md:order-2">
-         <Button className="w-12 h-10 inline text-indigo-800" color="gray" pill>
-            <FaMoon />
+         <Button className="w-12 h-10 inline text-indigo-800" color="gray" pill onClick={() => dispatch(toggleTheme())}>
+            {theme === 'light' ? <FaSun/> : <FaMoon/>}
          </Button>
          <Link to='/'>
             <Button gradientDuoTone='purpleToPink' outline>
@@ -23,25 +27,25 @@ const Header = () => {
       </div>
       <Navbar.Collapse>
          <Navbar.Link active={path === "/"} as={'div'}>
-            <Link to="/" className="text-indigo-800 font-semibold hover:text-indigo-900 hover:underline">Home</Link>
+            <Link to="/" className="text-indigo-800 font-semibold hover:text-indigo-900 dark:text-gray-400 dark:hover:text-[#ff014f] hover:underline">Home</Link>
          </Navbar.Link>
          <Navbar.Link active={path === "/about"} as={'div'}>
-            <Link to="/about" className="text-indigo-800 font-semibold hover:text-indigo-900 hover:underline">About Us</Link>
+            <Link to="/about" className="text-indigo-800 font-semibold hover:text-indigo-900 dark:text-gray-400 dark:hover:text-[#ff014f] hover:underline">About Us</Link>
          </Navbar.Link>
          <Navbar.Link active={path === "/services"} as={'div'}>
-            <Link to="/services" className="text-indigo-800 font-semibold hover:text-indigo-900 hover:underline">Our Services</Link>
+            <Link to="/services" className="text-indigo-800 font-semibold hover:text-indigo-900 dark:text-gray-400 dark:hover:text-[#ff014f] hover:underline">Our Services</Link>
          </Navbar.Link>
          <Navbar.Link active={path === "/partners"} as={'div'}>
-            <Link to="/partners" className="text-indigo-800 font-semibold hover:text-indigo-900 hover:underline">Our partners</Link>
+            <Link to="/partners" className="text-indigo-800 font-semibold hover:text-indigo-900 dark:text-gray-400 dark:hover:text-[#ff014f] hover:underline">Our partners</Link>
          </Navbar.Link>
          <Navbar.Link active={path === "/testimonials"} as={'div'}>
-            <Link to="/testimonials" className="text-indigo-800 font-semibold hover:text-indigo-900 hover:underline">Testimonials</Link>
+            <Link to="/testimonials" className="text-indigo-800 font-semibold hover:text-indigo-900 dark:text-gray-400 dark:hover:text-[#ff014f] hover:underline">Testimonials</Link>
          </Navbar.Link>
          <Navbar.Link active={path === "/chairmenMsg"} as={'div'}>
-            <Link to="/chairmenMsg" className="text-indigo-800 font-semibold hover:text-indigo-900 hover:underline">Chairman's Message</Link>
+            <Link to="/chairmenMsg" className="text-indigo-800 font-semibold hover:text-indigo-900 dark:text-gray-400 dark:hover:text-[#ff014f] hover:underline">Chairman's Message</Link>
          </Navbar.Link>
          <Navbar.Link active={path === "/contact"} as={'div'}>
-            <Link to="/contact" className="text-indigo-800 font-semibold hover:text-indigo-900 hover:underline">Contact Us</Link>
+            <Link to="/contact" className="text-indigo-800 font-semibold hover:text-indigo-900 dark:text-gray-400 dark:hover:text-[#ff014f] hover:underline">Contact Us</Link>
          </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
